bb731dcc437d2e7bdf3d3d9b78fff587cce7329d
tanguofu <tanguofu@gmail.com>
2014-06-27 18:08:41 +0800

    remove build tmp files

--- build/CMakeFiles/protobuf_include.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/protobuf_include"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/protobuf_include.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/mio2/CMakeFiles/mio2.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libmio2.a"
-)
